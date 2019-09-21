9627a7e0a40a89711f26a91788538b931ac3c45f
Didier Zuñiga <didier35@hotmail.com>
2019-09-21 14:14:49 -0500

    [Applied Redux]

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,66 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import gravatar from '../utils/gravatar';
+import { logoutRequest } from '../actions';
 import '../assets/styles/components/Header.scss';
 import userIcon from '../assets/static/user-icon.png';
 
-const Header = () => (
-  <header className="header">
-    <p className="header__logo">VideoPlayer</p>
-    <div className="header__menu">
-      <div className="header__menu--profile">
-        <img src={userIcon} alt="" />
-        <p>Perfil</p>
-      </div>
-      <ul>
-        <li><a href="/">Cuenta</a></li>
-        <li><a href="/">Cerrar Sesión</a></li>
-      </ul>
-    </div>
-  </header>
-);
+const Header = props => {
 
-export default Header;
+  const { user } = props;
+  const hashUser = Object.keys(user).length > 0;
+
+  const handleLogout = () => {
+    props.logoutRequest({})
+  }
+
+  return(
+    <header className="header">
+      <Link to='/'>
+        <p className="header__logo">VideoPlayer</p>
+      </Link>
+
+      <div className="header__menu">
+        <div className="header__menu--profile">
+          {
+            hashUser
+              ? <img src={gravatar(user.email)} alt={user.email} />
+              : <img src={userIcon} alt="" />
+          }
+          <p>Perfil</p>
+        </div>
+        <ul>
+          {
+            hashUser
+              ? <>
+                  <li><a href="/">{user.name}</a></li>
+                  <li>
+                    <a href="#logout" onClick={handleLogout}>
+                      Cerrar sesión
+                    </a>
+                  </li>
+                </>
+              : <li>
+                  <Link to="/login">
+                    Iniciar sesión
+                  </Link>
+                </li>
+          }
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+const mapStateToProps = state => {
+  return {
+    user: state.user
+  }
+}
+
+const mapDispatchToProps = {
+  logoutRequest
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
---
+++ src/containers/Register.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { registerRequest } from '../actions';
+import { Link } from 'react-router-dom';
+import '../assets/styles/components/Register.scss';
+
+const Register = props => {
+
+  const [form, setValues] = useState({
+    email: '',
+    name: '',
+    password: ''
+  });
+
+  const validateEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
+
+  const handleInput = event => {
+    // if ([event.target.name] == 'email') {
+    //   if (validateEmail(event.target.value)) {
+    //     console.log('Esta OK')
+    //   } else {
+    //     console.log('Esta WRONG')
+    //   }
+    // }
+    setValues({
+      ...form,
+      [event.target.name]: event.target.value
+    })
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    props.registerRequest(form);
+    props.history.push('/');
+  }
+
+  return (
+    <section className="register">
+      <section className="register__container">
+        <h2>Regístrate</h2>
+        <form className="register__container--form" onSubmit={handleSubmit}>
+          <input
+            name="name"
+            className="input"
+            type="text"
+            placeholder="Nombre"
+            onChange={handleInput}
+          />
+          <input
+            name="email"
+            className="input"
+            type="text"
+            placeholder="Correo"
+            onChange={handleInput}
+          />
+          <input
+            name="password"
+            className="input"
+            type="password"
+            placeholder="Contraseña"
+            onChange={handleInput}
+          />
+          <button className="button">Registrarme</button>
+        </form>
+        <Link to="/login">
+          Iniciar sesión
+        </Link>
+      </section>
+    </section>
+  )
+}
+
+const mapDispatchToProps = {
+  registerRequest
+}
+
+export default connect(null, mapDispatchToProps)(Register);
