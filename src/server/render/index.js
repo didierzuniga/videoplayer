724204c14e61aedee69893209e898c4ea989d308
Didier Zuñiga <didier35@hotmail.com>
2019-09-22 19:37:11 -0500

    [SSR finished]

---
+++ src/server/render/index.js
@@ -0,0 +1,46 @@
+import getManifest from '../getManifest';
+const dotenv = require('dotenv');
+
+dotenv.config();
+const isProd = (process.env.NODE_ENV === 'production');
+let srcs = {
+  mainCss: 'assets/app.css',
+  mainJs: 'assets/app.js',
+  vendorsJs: 'assets/vendor.js'
+}
+if (isProd) {
+  const files = getManifest();
+  srcs.mainCss = files['main.css']
+  srcs.mainJs = files['main.js']
+  srcs.vendorsJs = files['vendors.js']
+}
+
+const render = (html, preloadedState) => {
+  return (`
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Video Player</title>
+        <link rel="stylesheet" href="${srcs.mainCss}" type="text/css" />
+      </head>
+      <body>
+        <div id="app">${html}</div>
+        <script>
+          // WARNING: See the following for security issues around embedding JSON in HTML:
+          // http://redux.js.org/recipes/ServerRendering.html#security-considerations
+          window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(
+            /</g,
+            '\\u003c'
+          )}
+        </script>
+        <script src="${srcs.mainJs}" type="text/javascript"></script>
+        <script src="${srcs.vendorsJs}" type="text/javascript"></script>
+      </body>
+    </html>
+  `);
+}
+
+export default render;
--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import webpack from 'webpack';
+import main from './routes/main';
+import helmet from 'helmet';
 
 dotenv.config();
 
@@ -8,9 +10,10 @@ const ENV = process.env.NODE_ENV;
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(express.static(`${__dirname}/public`));
 
 if (ENV === 'development') {
-  console.log('Loading dev config...');
+  console.log('Loading development config...');
   const webpackConfig = require('../../webpack.config');
   const webpackDevMiddleware = require('webpack-dev-middleware');
   const webpackHotMiddleware = require('webpack-hot-middleware');
@@ -25,27 +28,14 @@ if (ENV === 'development') {
   };
   app.use(webpackDevMiddleware(compiler, serverConfig));
   app.use(webpackHotMiddleware(compiler));
+} else {
+  console.log(`Loading production config`);
+  app.use(helmet());
+  app.use(helmet.permittedCrossDomainPolicies());
+  app.disable('x-powered-by');
 }
 
-app.get('*', (req, res) => {
-  res.send(`
-    <!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Video Player</title>
-        <link rel="stylesheet" href="assets/app.css" type="text/css" />
-      </head>
-      <body>
-        <div id="app"></div>
-        <script src="assets/app.js" type="text/javascript"></script>
-        <script src="assets/vendor.js" type="text/javascript"></script>
-      </body>
-    </html>
-  `);
-})
+app.get('*', main)
 
 app.listen(PORT, (err) => {
   if (err) console.log(err);
