3941e0e7daef036da8dbde0061e99e961a1e992f
Didier Zuñiga <didier35@hotmail.com>
2019-11-02 22:21:14 -0500

    [Setting jest test to components and store]

--- src/server/routes/main.js
+++ src/server/routes/main.js
@@ -4,36 +4,64 @@ import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import { StaticRouter } from 'react-router'
 import { renderRoutes } from 'react-router-config'
+import axios from 'axios'
 import Routes from '../../frontend/routes/serverRoutes'
 import Layout from '../../frontend/components/Layout'
 import reducer from '../../frontend/reducers'
-import initialState from '../../frontend/initialState'
 import render from '../render'
-import { prototype } from 'stream';
+import { prototype } from 'stream'
 
-let initState = initialState
+require('dotenv').config()
 
-const main = (req, res, next) => {
+const main = async (req, res, next) => {
   try {
-    if (!(JSON.stringify(req.cookies) === '{}')) {
-      const { email, name, id } = req.cookies
-      initState = {
-        user: {
+    let initialState
+    try {
+      const { token, email, name, id } = req.cookies
+      let user = {}
+
+      if (email || name || id) {
+        user =  {
           id,
           email,
           name
-        },
+        }
+      }
+
+      let movieList = await axios({
+        url: `${process.env.API_URL}/api/movies`,
+        headers: { Authorization: `Bearer ${token}` },
+        method: 'get'
+      })
+
+      let userMovies = await axios({
+        url: `${process.env.API_URL}/api/movies?userId=${id}`,
+        headers: { Authorization: `Bearer ${token}` },
+        method: 'get'
+      })
+
+      movieList = movieList.data.data
+      userMovies = userMovies.data.data
+
+      initialState = {
+        user,
+        playing: {},
+        myList: userMovies.filter(movie => movie._id == id),
+        trends: movieList.filter(movie => movie.contentRating == 'PG' && movie.id),
+        originals: movieList.filter(movie => movie.contentRating == 'G' && movie.id)
+      }
+    } catch (error) {
+      initialState = {
+        user: {},
         playing: {},
         myList: [],
-        trends: [],
-        originals: []
+        trends: {},
+        originals: {}
       }
     }
-
-    console.log(initState.user)
     
-    const isLogged = (initState.user.id)
-    const store = createStore(reducer, initState)
+    const isLogged = (initialState.user.id)
+    const store = createStore(reducer, initialState)
     const html = renderToString(
       <Provider store={store}>
         <StaticRouter
